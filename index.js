fb7f8b961b7eb241265e58ae18093bd652484b12
Willian Praciano <willian.s.praciano@gmail.com>
2020-05-07 02:55:13 -0300

    Creating project and connecting to mongoDB

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+const app = express();
+
+dotenv.config();
+
+//Connect to mongoDB
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, 
+  ()=>{console.log('Conectado ao mongoDB')}
+);
+
+//Importing Routes
+const authRoute = require('./routes/auth');
+
+//Route Middlewares
+app.use('/api/user', authRoute);
+
+
+app.listen(3333);
