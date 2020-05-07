10a888be6e209bd0b53f4d910d511e55261d32aa
Willian Praciano <willian.s.praciano@gmail.com>
2020-05-07 17:46:40 -0300

    Adding jwt to auth and creating private routes with it

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 const {registerValidation, loginValidation} = require('../validation');
+const verify = require('./verifyToken');
 
+//REGISTER
 router.post('/register', async (req, res)=>{
   const {name, email, password} = req.body;
 
@@ -30,6 +33,38 @@ router.post('/register', async (req, res)=>{
   }catch(err){
     res.status(400).send(err);
   }
+
+});
+
+//LOGIN
+router.post('/login', async (req, res)=>{
+  const {email, password} = req.body;
+
+  //Validate data
+  const {error} = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //check if the email is in the database
+  const user = await User.findOne({email});
+  if(!user) return res.status(400).send('Email or password is wrong');
+
+  //check if password is correct
+  const validPass = await bcrypt.compare(password, user.password);
+  if(!validPass) return res.status(400).send('invalid password');
+
+  //Create and assign token
+  const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+  res.header('auth-token', token).send(token);
+
+  //res.send('Logged in!');
+
+});
+
+//LIST USER
+router.get('/list-users', verify ,async (req, res)=>{
+
+  const user = await User.find();
+  return res.json(user);
 
 });
 
