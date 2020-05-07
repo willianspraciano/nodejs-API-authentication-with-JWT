9d57975d6e9e5db6aa5c5b60d871f114522b90ce
Willian Praciano <willian.s.praciano@gmail.com>
2020-05-07 03:51:15 -0300

    Creating User model, implementing Route to register new user and validade data

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+//Importing Routes
+const authRoute = require('./routes/auth');
+
 const app = express();
 
 dotenv.config();
@@ -11,9 +14,8 @@ mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true },
   ()=>{console.log('Conectado ao mongoDB')}
 );
 
-//Importing Routes
-const authRoute = require('./routes/auth');
-
+//Middware
+app.use(express.json());
 //Route Middlewares
 app.use('/api/user', authRoute);
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,41 @@
 const router = require('express').Router();
+const User = require('../models/User');
 
-router.post('/register', (req, res)=>{
-  res.send('Register');
+//VALIDATION
+const Joi = require('@hapi/joi');
+
+const schema = Joi.object(
+  {
+    name: Joi.string()
+          .min(6)
+          .required(),
+    email: Joi.string()
+          .min(6)
+          .required()
+          .email(),
+    password: Joi.string()
+              .min(6)
+              .required(),
+  }
+);
+
+router.post('/register', async (req, res)=>{
+
+  //EASER WAY TO VALIDADE THE DATA
+  const {error} = schema.validate(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const {name, email, password} = req.body;
+
+  const user = new User({name, email, password,});
+  try{
+    const savedUser = await user.save();
+    res.send(savedUser);
+  }catch(err){
+    res.status(400).send(err);
+  }
+
 });
 
 module.exports = router;
